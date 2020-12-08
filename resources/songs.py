9c925c917826f520fb56ee47fbfbfb8dedbe40b2
Wayne Cho <waynecho2004@gmail.com>
2020-12-08 11:39:45 -0500

    update song

--- resources/songs.py
+++ resources/songs.py
@@ -11,6 +11,7 @@ from playhouse.shortcuts import model_to_dict
 # second argument is it's import_name
 song = Blueprint('songs', 'song')
 
+## INDEX ROUTE
 @song.route('/', methods=["GET"])
 def get_all_songs():
     ## find the songs and change each one to a dictionary into a new array
@@ -21,17 +22,33 @@ def get_all_songs():
     except models.DoesNotExist:
         return jsonify(data={}, status={"code": 401, "message": "Error getting the resources"})
 
+## POST ROUTE
 @song.route('/', methods=["POST"])
 def create_songs():
     ## see request payload anagolous to req.body in express
-    payload = request.get_json()
-    print(type(payload), 'payload')
-    song = models.Song.create(**payload)
-    ## see the object
-    print(song.__dict__)
-    ## Look at all the methods
-    print(dir(song))
-    # Change the model to a dict
-    print(model_to_dict(song), 'model to dict')
-    song_dict = model_to_dict(song)
-    return jsonify(data=song_dict, status={"code": 201, "message": "Success"})
+    try:
+        payload = request.get_json()
+        print(type(payload), 'payload')
+        song = models.Song.create(**payload)
+        ## see the object
+        print(song.__dict__)
+        ## Look at all the methods
+        print(dir(song))
+        # Change the model to a dict
+        print(model_to_dict(song), 'model to dict')
+        song_dict = model_to_dict(song)
+        return jsonify(data=song_dict, status={"code": 201, "message": "Success"})
+    except models.DoesNotExist:
+        return jsonify(data={}, status={"code": 401, "message": "Error creating a song"})
+
+## UPDATE ROUTE
+@song.route('/<id>', methods=["PUT"])
+def update_song(id):
+    try:
+        payload = request.get_json()
+        # update will update the selected fields, e.g. (artist)
+        query = models.Song.update(**payload).where(models.Song.id==id)  
+        query.execute()
+        return jsonify(data=model_to_dict(models.Song.get_by_id(id)), status={"code": 200, "message": "resource updated successfully"})
+    except models.DoesNotExist:
+        return jsonify(data={}, status={"code": 401, "message": "Error updating a song"})
