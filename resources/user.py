599426b869dc6bce0a3d177a12aef5cbf261c4eb
Wayne Cho <waynecho2004@gmail.com>
2020-12-09 18:29:32 -0500

    add user login()

--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,6 @@
 import models
 from flask import request, jsonify, Blueprint
-from flask_bcrypt import generate_password_hash
+from flask_bcrypt import generate_password_hash, check_password_hash
 from flask_login import login_user
 from playhouse.shortcuts import model_to_dict # this is peewee playhouse package
 
@@ -43,4 +43,22 @@ def register():
         del user_dict['password'] # delete the password before we return it, because we don't need the client to be aware of it
         return jsonify(data=user_dict, status={"code": 201, "message": "Success"})
 
+@user.route('/login', methods=["POST"])
+def login():
+    payload = request.get_json()
+    print(payload, '< --- this is playload')
+    try:
+        user = models.User.get(models.User.email== payload['email']) ### Try find the user by thier email
+        user_dict = model_to_dict(user) # the User model is converting into a dictionary so you can access it
+
+        # use bcyrpts check password to see if passwords match
+        if(check_password_hash(user_dict['password'], payload['password'])): 
+            del user_dict['password'] # delete the password
+            login_user(user) # setup the session
+            print(user, ' this is user')
+            return jsonify(data=user_dict, status={"code": 200, "message": "Success"}) # respond to the client
+        else:
+            return jsonify(data={}, status={"code": 401, "message": "Username or Password is incorrect"})
+    except models.DoesNotExist:
+        return jsonify(data={}, status={"code": 401, "message": "Username or Password is incorrect"})
 
