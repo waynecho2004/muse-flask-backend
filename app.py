8e9f1cc285ec80615c9c3defc6c1e6bae1a49109
Wayne Cho <waynecho2004@gmail.com>
2020-12-09 18:24:11 -0500

    register user blueprint and setup session in app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, g
 from flask_cors import CORS
-
-from resources.songs import song
+from flask_login import LoginManager
 
 import models
+
+# importing resources
+from resources.songs import song
+from resources.user import user
+
+# sets up the ability to set up the session
+login_manager = LoginManager()
 
 # wcho: set to False in prodution
 DEBUG = True 
@@ -12,6 +18,21 @@ PORT = 8000
 # Initialize an instance of the Flask class.
 # This starts the website!
 app = Flask(__name__)
+
+######### Add Following lines ######
+## Need secrete_key to encode the session
+app.secret_key = "LJAKLJLKJJLJKLSDJLKJASD" 
+# set up the sessions on the app
+login_manager.init_app(app) 
+# A decorator function, which will load the user object whenever we access the session, 
+# we can get the user by importing current_user from the flask_login
+@login_manager.user_loader 
+
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
 
 @app.before_request
 def before_request():
@@ -27,9 +48,12 @@ def after_request(response):
 
 # Add Cors support
 CORS(song, origins=['http://localhost:3000'], supports_credentials=True)
+CORS(user, origins=['http://localhost:3000'], supports_credentials=True)
 
-# register blueprint: in order to use the song blueprint in songs.py
+# register blueprint: in order to use the 
+#   song blueprint from songs.py and user blueprint from user.py
 app.register_blueprint(song, url_prefix='/api/v1/songs')
+app.register_blueprint(user, url_prefix='/user')
 
 # The default URL ends in / ("my-website.com/").
 @app.route('/')
@@ -38,5 +62,5 @@ def index():
 
 # Run the app when the program starts!
 if __name__ == '__main__':
-    models.initialize()   
+    models.initialize()
     app.run(debug=DEBUG, port=PORT)    
